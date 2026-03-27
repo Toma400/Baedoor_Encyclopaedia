6b4002f51bbe3784ab0ba1c38c120215af8637c5
Toma400 <thomasso_calgori@o2.pl>
2026-03-27 17:18:09 +0100

    Added `Doc Downloader` tool and updated several docs (+ added few more), now using automated system

---
+++ Tools/DocDownloader.py
@@ -0,0 +1,42 @@
+import requests
+
+DOCS = {
+    # general
+    "Lore Primer.pdf":                     "https://docs.google.com/document/d/1HYiXiSu3eDrKNwJxIkoAMJmHsUnIPbBKymwdrVUFVyM/export?format=pdf",
+    "Baedoor Magic.pdf":                   "https://docs.google.com/document/d/1DBIKW8WJfT-L35NVFz3ZjRxLLa7WyV39IT9xK39N4Jw/export?format=pdf",
+    # baedoorians
+    "Baedoor Island Regions.pdf":          "https://docs.google.com/document/d/1iAm7cgAdXASGp59WtBIVUJzXtk8kM8onvP3Ipc-zhYw/export?format=pdf",
+    "Baedoor Island History.pdf":          "https://docs.google.com/document/d/1oTYVx0SSAY5yDAPQAGF8iYhda2pfay-GMpLO9uQdzYk/export?format=pdf",
+    "Ansur Megadoc.pdf":                   "https://docs.google.com/document/d/1Wnqy_b-xHs4IbnqHv82h26AEgCbl5CMJRfnHfu8j8QI/export?format=pdf",
+    # goblins
+    "Goblins Megadoc.pdf":                 "https://docs.google.com/document/d/1odqSXnZXhanO7WpNob59066HJKGNfmvIQ2xRj-_jGF4/export?format=pdf",
+    # erds
+    "Erdic Megadoc.pdf":                   "https://docs.google.com/document/d/1r363t4fwMEPEzP8dQMidX3nHc3FsnesdcnCzdnQNDPg/export?format=pdf",
+    "Erdic Philosophy & Spiritualism.pdf": "https://docs.google.com/document/d/1iMKfX00HUoB9cIDj_HBhTJOM4X9OuWtksxwfL_38E-E/export?format=pdf",
+    "Erdis.pdf":                           "https://docs.google.com/document/d/14UvLjdrwHZYTowS0YWK61CkySBULTdz3lF5yC8Pb5Uc/export?format=pdf",
+    # pahtri
+    "Lai-Kine Religion.pdf":               "https://docs.google.com/document/d/18GTz8W-N58G3glFdjthv7OxwXE04SDP2_SqC-ajZ_4M/export?format=pdf",
+    # other
+    "Rev's Worldbuilding Questions.pdf":   "https://docs.google.com/document/d/1S9M-lykYW7in-PHx1-ADrIggCdQKR2_CqYs2mmXiqPo/export?format=pdf",
+}
+OUT = "../Docs/"
+
+def downloadDocs():
+    needle = 1
+    for file, link in DOCS.items():
+        print(f"Processing docs: {needle}/{len(DOCS)}")
+        response = requests.get(link)
+        if response.status_code == 200:
+            with open(OUT + file, 'wb') as f:
+                f.write(response.content)
+                #print('File saved to: {}'.format(OUT + file))
+        else:
+            print(f'Error downloading Google Doc: {file} | Error code: {response.status_code}')
+        needle += 1
+
+
+##############################################
+# delete docs (not LE folder!)
+downloadDocs()
+# do .git check
+# commit Docs folder
